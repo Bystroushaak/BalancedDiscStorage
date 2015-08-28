22a05f83c30f9f8ebe0b51de52f4f2e5c80cb091
Bystroushaak <bystrousak@kitakitsune.org>
2015-08-28 17:36:35 +0200

    #1: Added fixtures for all test data files.

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -20,10 +20,55 @@ from BalancedDiscStorage import BalancedDiscStorage
 TEMP_DIR = ""
 
 
+# Functions ===================================================================
+def data_dir_context(filename):
+    return join(
+        os.path.dirname(__file__),
+        "files",
+        filename
+    )
+
+
+def data_file_context(filename):
+    return open(
+        data_dir_context(filename)
+    )
+
+
 # Fixtures ====================================================================
-# @pytest.fixture
-# def fixture():
-#     pass
+@pytest.fixture
+def bds():
+    return BalancedDiscStorage(TEMP_DIR)
+
+
+@pytest.fixture
+def a_file():
+    return data_file_context("a_file")
+
+
+@pytest.fixture
+def aa_file():
+    return data_file_context("aa_file")
+
+
+@pytest.fixture
+def b_file():
+    return data_file_context("b_file")
+
+
+@pytest.fixture
+def a_file_hash():
+    return "aea92132c4cbeb263e6ac2bf6c183b5d81737f179f21efdc5863739672f0f470"
+
+
+@pytest.fixture
+def b_file_hash():
+    return "b17ef6d19c7a5b1ee83b907c595526dcb1eb06db8227d650d5dda0a9f4ce8cd9"
+
+
+@pytest.fixture
+def aa_file_hash():
+    return "aae02129362d611717b6c00ad8d73bf820a0f6d88fca8e515cafe78d3a335965"
 
 
 # Tests =======================================================================
