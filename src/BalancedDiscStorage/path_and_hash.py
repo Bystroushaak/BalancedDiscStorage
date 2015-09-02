06cf333722ca1b5bf963a4f006ba121fa8b14cdc
Bystroushaak <bystrousak@kitakitsune.org>
2015-09-02 17:31:12 +0200

    #7: `file_hash` argument renamed to `hash`. Added note about usage.

--- src/BalancedDiscStorage/path_and_hash.py
+++ src/BalancedDiscStorage/path_and_hash.py
@@ -11,16 +11,23 @@ class PathAndHash(str):
     """
     Path representation, which also holds hash.
 
+    Note:
+        This class is based on `str`, with which is fully interchangeable.
+
+            str(PathAndHash(path="xe", hash="asd")) == "xe"
+
     Attributes:
         path (str): Path to the file.
         hash (str): Hash of the file.
     """
-    def __new__(self, path, file_hash=None):
+    def __new__(self, path, hash=None):
         return super(PathAndHash, self).__new__(self, path)
 
-    def __init__(self, path, file_hash=None):
+    def __init__(self, path, hash=None):
+        super(PathAndHash, self).__init__(path)
+
         self.path = path
-        self.hash = file_hash
+        self.hash = hash
 
     def __repr__(self):
         return self.path
